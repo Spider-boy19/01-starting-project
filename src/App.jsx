ac9dc65d27f63fb04fc4a89d8f1dd6ebc70f037e
Spider-boy19 <Chrisematt04@gmail.com>
2026-04-27 12:38:26 -0400

    fix issues with website

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS , EXAMPLES} from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcepts/CoreConcept';
 import TabButton from './components/TabButton/TabButton';
+import { useState } from 'react';
 // function UserData(){
 //   const user = {first:"George",last:"Salayka", age:"40" };
 
@@ -14,6 +15,26 @@ import TabButton from './components/TabButton/TabButton';
 // }
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  let tabContent = <p>Plese Seclect a topic</p>;
+  if(selectedTopic){
+    tabContent = (
+                <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic.code]}
+              </code>
+            </pre>
+          </div>
+    )
+  }
+
+  function handleSelect(selectedbutton){
+    setSelectedTopic(selectedbutton);
+   console.log(selectedTopic);
+  }
   return (
     <div>
       <Header />
@@ -21,39 +42,22 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept
-              {
-              ...CORE_CONCEPTS[0]
-              }
-            />
-            <CoreConcept
-              {
-              ...CORE_CONCEPTS[1]
-              }
-            />
-            <CoreConcept
-              {
-              ...CORE_CONCEPTS[2]
-              }
-            />
-            <CoreConcept
-              {
-              ...CORE_CONCEPTS[3]
-              }
-            />
             {
-              // CORE_CONCEPTS.map((concept)=><CoreConcept {...concept}/>)
+              CORE_CONCEPTS.map((conceptItem)=>{
+                <CoreConcept key={conceptItem.title} {...conceptItem} />
+              })
             }
           </ul>
         </section>
         <section> id="examples"
           <h2>Examples</h2>
           <menu>
-            <TabButton>Componets</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton> isSelected={selectedTopic==="componets"} onSelect={()=>handleSelect("components")}</TabButton>
+            <TabButton> isSelected={selectedTopic==="jsx"} onSelect={()=>handleSelect("jsx")}</TabButton>
+            <TabButton> isSelected={selectedTopic==="props"} onSelect={()=>handleSelect("props")}</TabButton>
+            <TabButton> isSelected={selectedTopic==="state"} onSelect={()=>handleSelect("state")}</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,10 +1,12 @@
 import './TabButton.css';
 
-export default function TabButton(){
+export default function TabButton({children, onSelect}){
     function handleClick(){
         console.loh('Hello World');
     }
-    return{
-        
-    }
+    return(
+        <li>
+            <button className = {isSelected ? 'active' : ''} onClick={onselect}>{children}</button>
+        </li>
+    )
 }
